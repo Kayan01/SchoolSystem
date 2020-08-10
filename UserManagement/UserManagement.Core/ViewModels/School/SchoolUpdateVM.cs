7fb076ed84bf8906b4c9f2c8d458dd2b1eb0f258
Frank Ofoedu <fofoedu@sbsc.com>
2020-08-10 12:30:05 +0100

    added school view models

---
+++ UserManagement/UserManagement.Core/ViewModels/School/SchoolUpdateVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using UserManagement.Core.Models;
+
+namespace UserManagement.Core.ViewModels
+{
+   public class SchoolUpdateVM
+    {   
+        [Required]
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public static implicit operator SchoolUpdateVM(School model)
+        {
+            return model == null ? null : new SchoolUpdateVM
+            {
+                Id = model.Id,
+                Name = model.Name
+            };
+        }
+
+    }
+}
---
+++ UserManagement/UserManagement.Core/ViewModels/School/SchoolVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserManagement.Core.Models;
+
+namespace UserManagement.Core.ViewModels
+{
+   public class SchoolVM
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public static implicit operator SchoolVM(School model)
+        {
+            return model == null ? null : new SchoolVM
+            {
+                Id = model.Id,
+                Name = model.Name
+            };
+        }
+
+    }
+}
