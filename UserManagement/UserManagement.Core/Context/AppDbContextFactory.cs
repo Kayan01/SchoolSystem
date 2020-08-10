df4318e9c6d1f61ec6fad90378e230b7ca86be49
Frank Ofoedu <fofoedu@sbsc.com>
2020-08-10 12:12:28 +0100

    added context files

---
+++ UserManagement/UserManagement.Core/Context/AppDbContextFactory.cs
@@ -0,0 +1,11 @@
+﻿using Shared.DataAccess.EfCore.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserManagement.Core.Context
+{
+    public class AppDbContextFactory : AppDbContextFactory<AppDbContext>
+    {
+    }
+}
