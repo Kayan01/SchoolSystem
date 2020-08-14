e0088339192c4a1dd1c2cfc54fba5b065d32b9a7
Frank Ofoedu <fofoedu@sbsc.com>
2020-08-14 09:43:14 +0100

    added class vms

---
+++ UserManagement/UserManagement.Core/ViewModels/SchoolClass/ClassVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserManagement.Core.ViewModels.Student;
+
+namespace UserManagement.Core.ViewModels.SchoolClass
+{
+    public class ClassVM
+    {
+        public long Id { get; internal set; }
+        public string  Name { get; set; }
+        public List<StudentVM> StudentVMs { get; set; }
+
+        public static implicit operator ClassVM(Models.SchoolClass model)
+        {
+            return model == null ? null : new ClassVM
+            {
+                Id = model.Id,
+                Name = model.Name,
+                StudentVMs = (List<StudentVM>)model.Students
+            };
+        }
+    }
+
+
+}
