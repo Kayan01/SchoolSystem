ef441b3584e802c1c7b902d575c9e5d706ffdf4e
Frank Ofoedu <fofoedu@sbsc.com>
2020-08-28 19:11:20 +0100

    model users update

---
+++ Auth.Core/Models/Users/Student.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Entities.Auditing;
+using Shared.Tenancy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Auth.Core.Models
+{
+    public class Student : Person
+    {
+        public int UserId { get; set; }
+        public long? ClassId { get; set; }
+        public SchoolClass Class { get; set; }
+    }
+}
