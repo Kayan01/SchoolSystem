459972a76a9b27b9524c1e83e56081dad3398fd8
Frank Ofoedu <fofoedu@sbsc.com>
2020-08-31 15:20:25 +0100

    class, class groups, school sections models, mappings and other data

---
+++ Auth.Core/Models/SchoolSection.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Entities.Auditing;
+using Shared.Tenancy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auth.Core.Models
+{
+    /// <summary>
+    /// Defines the school sections such as Secondary, primary , nursery etc
+    /// </summary>
+    public class SchoolSection : AuditedEntity<long>, ITenantModelType
+    {
+        public long TenantId { get; set; }
+        public string Name { get; set; }
+    }
+}
