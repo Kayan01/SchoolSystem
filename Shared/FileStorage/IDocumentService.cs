56f371137f0cd0aa64e2ed22bb04d98209ec09b1
Frank Ofoedu <fofoedu@sbsc.com>
2020-09-02 14:04:16 +0100

    document service, vm and DI registration

---
+++ Shared/FileStorage/IDocumentService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shared.Entities;
+using Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.FileStorage
+{
+   public interface IDocumentService
+    {
+        List<FileUpload> TryUploadSupportingDocuments(List<DocumentVM> formFiles);
+    }
+}
