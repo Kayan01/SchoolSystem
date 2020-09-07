8549f41bd6d6adbf37c3c66ca3b5b023b585c187
Frank Ofoedu <fofoedu@sbsc.com>
2020-09-07 12:29:51 +0100

    refactored school service

---
+++ Auth.Core/Interfaces/School/ISchoolService.cs
@@ -0,0 +1,19 @@
+﻿using Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Auth.Core.ViewModels;
+using Auth.Core.ViewModels.School;
+
+namespace Auth.Core.Services.Interfaces
+{
+   public interface ISchoolService
+    {
+        Task<ResultModel<List<SchoolVM>>> GetAllSchools(int pageNumber, int pageSize);
+        Task<ResultModel<CreateSchoolVM>> AddSchool(CreateSchoolVM model);
+        Task<ResultModel<SchoolVM>> GetSchoolById(long Id);
+        Task<ResultModel<SchoolUpdateVM>> UpdateSchool(SchoolUpdateVM model);
+        Task<ResultModel<bool>> DeleteSchool(long Id);
+    }
+}
