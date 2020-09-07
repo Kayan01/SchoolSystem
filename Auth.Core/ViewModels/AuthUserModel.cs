08fe7b9c1a145431d798aeec95454730fbca9f68
Frank Ofoedu <fofoedu@sbsc.com>
2020-09-07 12:41:17 +0100

    auth service refactor and users update

---
+++ Auth.Core/ViewModels/AuthUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auth.Core.ViewModels
+{
+    public class AuthUserModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
