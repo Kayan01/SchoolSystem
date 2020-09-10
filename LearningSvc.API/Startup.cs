6e4e36c57a0657cc71066c201d5f4e513a6e4d38
Kingsley Ekoh-Ordan <keordan@sbsc.com>
2020-09-10 22:33:34 +0100

    feature: add API Gateway

--- LearningSvc.API/Startup.cs
+++ LearningSvc.API/Startup.cs
@@ -10,8 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using LearningSvc.API.Utils;
 using LearningSvc.Core.Context;
+using Shared.Utils;
 
 namespace LearningSvc.API
 {
