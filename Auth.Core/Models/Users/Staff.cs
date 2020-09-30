c19926b13f56911ec9ac0bd88b72e07666f02108
Frank Ofoedu <fofoedu@sbsc.com>
2020-09-30 09:26:57 +0100

    bug fix

--- Auth.Core/Models/Users/Staff.cs
+++ Auth.Core/Models/Users/Staff.cs
@@ -1,9 +1,11 @@
-﻿using Shared.Enums;
+﻿using Auth.Core.Models.Users;
+using Shared.Enums;
 
 namespace Auth.Core.Models
 {
     public class Staff : Person
     {
         public StaffType StaffType { get; set; }
+        public TeachingStaff TeachingStaff { get; set; }
     }
 }
