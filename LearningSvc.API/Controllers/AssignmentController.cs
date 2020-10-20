97a8a3deaa0207253f11a7c8c9115b83b0f69ad0
Okpara Obinna <obainoforgod@gmail.com>
2020-10-20 21:00:43 +0100

    Assignment controller

---
+++ LearningSvc.API/Controllers/AssignmentController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningSvc.Core.Services.Interfaces;
+using LearningSvc.Core.ViewModels.Assignment;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.AspNetCore;
+using Shared.ViewModels;
+using Shared.ViewModels.Enums;
+
+namespace LearningSvc.API.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
+    public class AssignmentController : BaseController
+    {
+        private readonly IAssignmentService _aService;
+        public AssignmentController(IAssignmentService aService)
+        {
+            _aService = aService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> GetAllAssignmentsByTeacher(long teacherId)
+        {
+            try
+            {
+                var result = await _aService.GetAssignmentsForTeacher(teacherId);
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> GetAllAssignmentAnswers(long assignmentId)
+        {
+            try
+            {
+                var result = await _aService.GetAllSubmission(assignmentId);
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> GetAssignmentAnswer(long answerId)
+        {
+            try
+            {
+                var result = await _aService.GetAssignmentSubmission(answerId);
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> UploadAssignment(AssignmentUploadVM model)
+        {
+            if (model == null)
+                return ApiResponse<string>(errors: "Empty payload");
+
+            if (!ModelState.IsValid)
+                return ApiResponse<object>(ListModelErrors, codes: ApiResponseCodes.INVALID_REQUEST);
+
+            try
+            {
+                var result = await _aService.AddAssignment(model);
+
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> UpdateScore(AssignmentSubmissionUpdateScoreVM model)
+        {
+            if (model == null)
+                return ApiResponse<string>(errors: "Empty payload");
+
+            if (!ModelState.IsValid)
+                return ApiResponse<object>(ListModelErrors, codes: ApiResponseCodes.INVALID_REQUEST);
+
+            try
+            {
+                var result = await _aService.UpdateScore(model);
+
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> UpdateComment(AssignmentSubmissionUpdateCommentVM model)
+        {
+            if (model == null)
+                return ApiResponse<string>(errors: "Empty payload");
+
+            if (!ModelState.IsValid)
+                return ApiResponse<object>(ListModelErrors, codes: ApiResponseCodes.INVALID_REQUEST);
+
+            try
+            {
+                var result = await _aService.UpdateComment(model);
+
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+    }
+}
