7651846427b602ff1a4790823c3e82f387be2d23
Okpara Obinna <obainoforgod@gmail.com>
2020-10-20 20:54:49 +0100

    Assignment view models

---
+++ LearningSvc.Core/ViewModels/Assignment/AssignmentUploadVM.cs
@@ -0,0 +1,22 @@
+﻿using Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningSvc.Core.ViewModels.Assignment
+{
+    public class AssignmentUploadVM
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+        public long SubjectId { get; set; }
+        public long ClassId { get; set; }
+        public long TeacherId { get; set; }
+
+        public DateTime DueDate { get; set; }
+        public int TotalScore { get; set; }
+        public List<DocumentVM> Documents { get; set; }
+
+    }
+}
