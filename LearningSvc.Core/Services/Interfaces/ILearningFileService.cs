3affd14d45a47c6d9bfce9aa5a9d4eaaa84260ed
Okpara Obinna <obainoforgod@gmail.com>
2020-10-22 18:49:55 +0100

    Added learningFile service and interface

---
+++ LearningSvc.Core/Services/Interfaces/ILearningFileService.cs
@@ -0,0 +1,17 @@
+﻿using LearningSvc.Core.ViewModels.LearningFiles;
+using Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningSvc.Core.Services.Interfaces
+{
+    public interface ILearningFileService
+    {
+        Task<ResultModel<List<LearningFileListVM>>> GetAllFileByTeacher(long teacherId);
+        Task<ResultModel<List<LearningFileListVM>>> GetAllFileByClass(long classId);
+        Task<ResultModel<string>> UploadLearningFile(LearningFileUploadVM model);
+        Task<ResultModel<string>> DeleteLearningFile(long fileId);
+    }
+}
