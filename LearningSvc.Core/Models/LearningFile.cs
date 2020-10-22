2515dfed208a0a95891741da7866d7f145f372f0
Okpara Obinna <obainoforgod@gmail.com>
2020-10-22 18:47:46 +0100

    Added File models

---
+++ LearningSvc.Core/Models/LearningFile.cs
@@ -0,0 +1,26 @@
+﻿using LearningSvc.Core.Enumerations;
+using Shared.Entities;
+using Shared.Entities.Auditing;
+using Shared.Tenancy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningSvc.Core.Models
+{
+    public class LearningFile : AuditedEntity<long>, ITenantModelType
+    {
+        public long TenantId { get; set; }
+        public long SchoolClassId { get; set; }
+        public long SubjectId { get; set; }
+        public long TeacherId { get; set; }
+        public Guid FileUploadId { get; set; }
+
+        public LearningFileType FileType { get; set; }
+
+        public Teacher Teacher { get; set; }
+        public Subject Subject { get; set; }
+        public SchoolClass SchoolClass { get; set; }
+        public FileUpload File { get; set; }
+    }
+}
