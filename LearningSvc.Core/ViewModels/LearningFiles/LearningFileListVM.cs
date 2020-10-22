d917b452f3cfb2fe940191ea5d1c098cfe1a4c36
Okpara Obinna <obainoforgod@gmail.com>
2020-10-22 18:45:47 +0100

    Filemanager viewModels

---
+++ LearningSvc.Core/ViewModels/LearningFiles/LearningFileListVM.cs
@@ -0,0 +1,50 @@
+﻿using LearningSvc.Core.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearningSvc.Core.ViewModels.LearningFiles
+{
+    public class LearningFileListVM
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string ClassName { get; set; }
+        public string TeacherName { get; set; }
+        public string SubjectName { get; set; }
+        public LearningFileType FileType { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string FileURL { get; set; }
+
+        public static implicit operator LearningFileListVM(Models.LearningFile model)
+        {
+            return model == null ? null : new LearningFileListVM
+            {
+                Id = model.Id,
+                Name = model.File?.Name,
+                ClassName = model.SchoolClass?.Name,
+                CreationDate = model.CreationTime,
+                FileType = model.FileType,
+                FileURL = model.File?.Path,
+                SubjectName = model.Subject?.Name,
+                TeacherName = $"{model.Teacher?.FirstName} {model.Teacher?.LastName}",
+            };
+        }
+
+        public static implicit operator LearningFileListVM(Models.Assignment model)
+        {
+            return model == null ? null : new LearningFileListVM
+            {
+                Id = model.Id,
+                Name = model.Attachments?.FirstOrDefault().Name,
+                ClassName = model.SchoolClass?.Name,
+                CreationDate = model.CreationTime,
+                FileType = LearningFileType.Assignment,
+                FileURL = model.Attachments?.FirstOrDefault().Path,
+                SubjectName = model.Subject?.Name,
+                TeacherName = $"{model.Teacher?.FirstName} {model.Teacher?.LastName}",
+            };
+        }
+    }
+}
---
+++ LearningSvc.Core/ViewModels/LearningFiles/LearningFileUploadVM.cs
@@ -0,0 +1,18 @@
+﻿using LearningSvc.Core.Enumerations;
+using Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningSvc.Core.ViewModels.LearningFiles
+{
+    public class LearningFileUploadVM
+    {
+        public string Name { get; set; }
+        public long ClassId { get; set; }
+        public long TeacherId { get; set; }
+        public long SubjectId { get; set; }
+        public LearningFileType FileType { get; set; }
+        public DocumentVM FileObj { get; set; }
+    }
+}
