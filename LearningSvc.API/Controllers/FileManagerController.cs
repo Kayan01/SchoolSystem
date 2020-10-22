ec2945e3c8198e5c70aed446b6aee568e2dba402
Okpara Obinna <obainoforgod@gmail.com>
2020-10-22 18:53:14 +0100

    FileManager controller

---
+++ LearningSvc.API/Controllers/FileManagerController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningSvc.Core.Services.Interfaces;
+using LearningSvc.Core.ViewModels.LearningFiles;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.AspNetCore;
+using Shared.ViewModels;
+using Shared.ViewModels.Enums;
+
+namespace LearningSvc.API.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
+    public class FileManagerController : BaseController
+    {
+        private readonly ILearningFileService _lService;
+        public FileManagerController(ILearningFileService lService)
+        {
+            _lService = lService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> GetAllFileByTeacher(long teacherId)
+        {
+            try
+            {
+                var result = await _lService.GetAllFileByTeacher(teacherId);
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> GetAllFileByClass(long classId)
+        {
+            try
+            {
+                var result = await _lService.GetAllFileByClass(classId);
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> UploadFile(LearningFileUploadVM model)
+        {
+            if (model == null)
+                return ApiResponse<string>(errors: "Empty payload");
+
+            if (!ModelState.IsValid)
+                return ApiResponse<object>(ListModelErrors, codes: ApiResponseCodes.INVALID_REQUEST);
+
+            try
+            {
+                var result = await _lService.UploadLearningFile(model);
+
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        //[Authorize]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> DeleteFile(long id)
+        {
+            if (id < 1)
+            {
+                return ApiResponse<string>(errors: "Please provide valid class Arm Id");
+            }
+
+            try
+            {
+                var result = await _lService.DeleteLearningFile(id);
+
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+    }
+}
