cbcc4fbf38ae2c67f42ba193d6b9d95e4b6c4ccc
Okpara Obinna <obainoforgod@gmail.com>
2020-10-30 10:01:59 +0100

    Remove learningfile interface, added interface for the file types

---
+++ LearningSvc.Core/Interfaces/ILessonNoteService.cs
@@ -0,0 +1,19 @@
+﻿using LearningSvc.Core.Enumerations;
+using LearningSvc.Core.ViewModels.LessonNote;
+using Shared.Utils;
+using Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningSvc.Core.Interfaces
+{
+    public interface ILessonNoteService
+    {
+        Task<ResultModel<PaginatedList<LessonNoteListVM>>> GetAllFileByTeacher(long teacherId, int pagenumber, int pagesize);
+        Task<ResultModel<PaginatedList<LessonNoteListVM>>> GetAllFileByClass(long classId, int pagenumber, int pagesize);
+        Task<ResultModel<string>> UploadLearningFile(LessonNoteUploadVM model);
+        Task<ResultModel<string>> DeleteLearningFile(long fileId);
+    }
+}
---
+++ LearningSvc.Core/Interfaces/IMediaService.cs
@@ -0,0 +1,19 @@
+﻿using LearningSvc.Core.Enumerations;
+using LearningSvc.Core.ViewModels.Media;
+using Shared.Utils;
+using Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningSvc.Core.Interfaces
+{
+    public interface IMediaService
+    {
+        Task<ResultModel<PaginatedList<MediaListVM>>> GetAllFileByTeacher(long teacherId, int pagenumber, int pagesize);
+        Task<ResultModel<PaginatedList<MediaListVM>>> GetAllFileByClass(long classId, int pagenumber, int pagesize);
+        Task<ResultModel<string>> UploadLearningFile(MediaUploadVM model);
+        Task<ResultModel<string>> DeleteLearningFile(long fileId);
+    }
+}
