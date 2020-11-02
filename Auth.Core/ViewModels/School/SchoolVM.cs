a4b3c9611fe35f6de4a5d3135d6fd75d88294dfe
Frank Ofoedu <fofoedu@sbsc.com>
2020-11-02 17:01:54 +0100

    changed user count implementation

--- Auth.Core/ViewModels/School/SchoolVM.cs
+++ Auth.Core/ViewModels/School/SchoolVM.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
 using Auth.Core.Models;
 using Microsoft.AspNetCore.Http;
 
 namespace Auth.Core.ViewModels
 {
-   public class SchoolVM
+    public class SchoolVM
     {
         public long Id { get; internal set; }
         public string Name { get; set; }
         public string ClientCode { get; set; }
-        public long UsersCount { get; set; }
+        public long? UsersCount { get; set; }
         public string State { get; set; }
         public bool Status { get; set; }
         public DateTime DateCreated { get; set; }
@@ -23,11 +24,11 @@ namespace Auth.Core.ViewModels
             {
                 Id = model.Id,
                 Name = model.Name,
-                 DateCreated = model.CreationTime,
-                  State = model.State,
-                    UsersCount = (model.Staffs?.Count + model.Students?.Count + model.TeachingStaffs?.Count).Value,
-                     ClientCode = model.ClientCode
-                    
+                DateCreated = model.CreationTime,
+                State = model.State,
+                UsersCount = model.Staffs.Count() + model.TeachingStaffs.Count() + model.Students.Count(),
+                ClientCode = model.ClientCode
+
             };
         }
 
