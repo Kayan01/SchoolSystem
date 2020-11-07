21bc524275e92ff615d004ec0eb180da36089d01
Okpara Obinna <obainoforgod@gmail.com>
2020-11-07 09:14:04 +0100

    Controller, Model, Service, Interface, and viewModels for managing the link between teacher and classSubject.

---
+++ LearningSvc.API/Controllers/TeacherClassSubjectController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningSvc.Core.Interfaces;
+using LearningSvc.Core.ViewModels.TeacherClassSubject;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.AspNetCore;
+using Shared.ViewModels;
+using Shared.ViewModels.Enums;
+
+namespace LearningSvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherClassSubjectController : BaseController
+    {
+        private readonly ITeacherClassSubjectService _teacherClassSubjectService;
+        public TeacherClassSubjectController(ITeacherClassSubjectService teacherClassSubjectService)
+        {
+            _teacherClassSubjectService = teacherClassSubjectService;
+        }
+
+        [HttpGet("{teacherid}")]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> GetAllClassSubjectsForTeacher(long teacherid)
+        {
+            try
+            {
+                var result = await _teacherClassSubjectService.GetAllTeacherClassSubjects(teacherid);
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ApiResponse<object>), 200)]
+        public async Task<IActionResult> AddSubjectsToClass([FromForm] TeacherClassSubjectInsertVM model)
+        {
+            if (model == null)
+                return ApiResponse<string>(errors: "Empty payload");
+
+            if (!ModelState.IsValid)
+                return ApiResponse<object>(ListModelErrors, codes: ApiResponseCodes.INVALID_REQUEST);
+
+            try
+            {
+                var result = await _teacherClassSubjectService.AddTeacherToClassSubjects(model);
+
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+                return ApiResponse<object>(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+    }
+}
