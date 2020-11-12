7c38c9b72b1719711ad091ae624440bd2d16037e
Okpara Obinna <obainoforgod@gmail.com>
2020-11-12 16:54:38 +0100

    Removed implicit operator

--- LearningSvc.Core/ViewModels/TimeTable/ClassSessionOutputVM.cs
+++ LearningSvc.Core/ViewModels/TimeTable/ClassSessionOutputVM.cs
@@ -11,30 +11,17 @@ namespace LearningSvc.Core.ViewModels.TimeTable
         public string ClassName { get; internal set; }
         public string TeacherName { get; internal set; }
         public string SubjectName { get; internal set; }
-        public TimeSpan? TimeFrom { get; internal set; }
-        public TimeSpan? TimeTo { get; internal set; }
+        public TimeSpan TimeFrom { get; internal set; }
+        public double DurationInMinutes { get; set; }
+        public int NoOfPeriods { get; set; } = 1;
+        public TimeSpan TimeTo { get {
+                return DurationInMinutes == 0d ? TimeSpan.Zero :
+                    TimeFrom.Add(TimeSpan.FromMinutes((DurationInMinutes * NoOfPeriods)));
+            } }
         public string PeriodName { get; internal set; }
         public int? NoOfStudent { get; set; }
         public bool HasVirtual { get; internal set; }
         public string ZoomId { get; internal set; }
 
-
-        public static implicit operator ClassSessionOutputVM(TimeTableCell model)
-        {
-            return model == null ? null : new ClassSessionOutputVM
-            {
-                TimeTableCellId = model.Id,
-                ClassName = model.SchoolClass?.Name,
-                SubjectName = model.Subject?.Name,
-                TeacherName = ($"{model.Teacher?.FirstName} {model.Teacher?.LastName}").Trim(),
-                TimeFrom = model.Period?.TimeFrom,
-                TimeTo = model.Period == null ? null : 
-                    model.Period?.TimeFrom.Add(TimeSpan.FromMinutes((model.Period?.DurationInMinutes * model.NoOfPeriod).Value)),
-                PeriodName = model.Period?.Name,
-                NoOfStudent = model.SchoolClass?.Students?.Count,
-                HasVirtual = model.HasVirtual,
-                ZoomId = model.ZoomId
-            };
-        }
     }
 }
