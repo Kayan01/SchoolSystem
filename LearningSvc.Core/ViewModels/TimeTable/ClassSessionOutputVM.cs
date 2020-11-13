d3a59c91c2b83ff5660e0afec0fa7cac5254baa6
Okpara Obinna <obainoforgod@gmail.com>
2020-11-13 14:48:32 +0100

    Updated VM

--- LearningSvc.Core/ViewModels/TimeTable/ClassSessionOutputVM.cs
+++ LearningSvc.Core/ViewModels/TimeTable/ClassSessionOutputVM.cs
@@ -1,6 +1,7 @@
 ﻿using LearningSvc.Core.Models.TimeTable;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace LearningSvc.Core.ViewModels.TimeTable
@@ -12,12 +13,17 @@ namespace LearningSvc.Core.ViewModels.TimeTable
         public string TeacherName { get; internal set; }
         public string SubjectName { get; internal set; }
         public TimeSpan TimeFrom { get; internal set; }
-        public double DurationInMinutes { get; set; }
+        public TimeSpan TimeTo { get; set; }
+
+        public double DurationInMinutes
+        {
+            get
+            {
+                return (TimeTo - TimeFrom).TotalMinutes;
+            }
+        }
+
         public int NoOfPeriods { get; set; } = 1;
-        public TimeSpan TimeTo { get {
-                return DurationInMinutes == 0d ? TimeSpan.Zero :
-                    TimeFrom.Add(TimeSpan.FromMinutes((DurationInMinutes * NoOfPeriods)));
-            } }
         public string PeriodName { get; internal set; }
         public int? NoOfStudent { get; set; }
         public bool HasVirtual { get; internal set; }
