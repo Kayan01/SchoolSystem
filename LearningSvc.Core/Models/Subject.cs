32858d40caecd0a333a13a0b76c474e0ca0e3c9c
Okpara Obinna <obainoforgod@gmail.com>
2020-11-30 13:34:46 +0100

    removed unnecessary navigation property

--- LearningSvc.Core/Models/Subject.cs
+++ LearningSvc.Core/Models/Subject.cs
@@ -12,7 +12,6 @@ namespace LearningSvc.Core.Models
         public string Name { get; set; }
         public bool IsActive { get; set; }
 
-        public ICollection<Assignment> Assignments { get; set; }
         public ICollection<SchoolClassSubject> SchoolClassSubjects { get; set; }
     }
 }
