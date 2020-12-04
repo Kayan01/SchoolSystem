9bfbb2703a7f4a9e0b1eb5370608dd1cf7daddc5
Okpara Obinna <obainoforgod@gmail.com>
2020-12-04 08:05:21 +0100

    Assessment models

---
+++ AssessmentSvc.Core/Models/Map/TeacherMap.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearningSvc.Core.Models.Map
+{
+    public class TeacherMap : IEntityTypeConfiguration<Teacher>
+    {
+        public void Configure(EntityTypeBuilder<Teacher> builder)
+        {
+            builder.Property(x => x.Id)
+                .ValueGeneratedNever();
+        }
+    }
+}
---
+++ AssessmentSvc.Core/Models/Result.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Shared.Entities.Auditing;
+using Shared.Tenancy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AssessmentSvc.Core.Models
+{
+    public class Result : AuditedEntity<long>, ITenantModelType
+    {
+        public long TenantId { get; set; }
+
+        public long StudentId { get; set; }
+        public long SessionId { get; set; }
+        public long SchoolClassId { get; set; }
+        public long SubjectId { get; set; }
+        public int TermSequenceNumber { get; set; }
+
+        public string ScoresJSON { get; private set; } //will contain List of Term object as JSON string
+
+        [NotMapped]
+        public List<Score> Scores
+        {
+            get
+            {
+                return JsonConvert.DeserializeObject<List<Score>>(ScoresJSON, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            }
+
+            set
+            {
+                ScoresJSON = JsonConvert.SerializeObject(value, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            }
+        }
+    }
+
+    public class Score
+    {
+        public string AssessmentName { get; set; }
+        public double StudentScore { get; set; }
+    }
+}
