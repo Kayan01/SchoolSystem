fef4a200a6ff08342801aef7ddb2d8b3afafa3ff
Frank Ofoedu <fofoedu@sbsc.com>
2020-12-06 11:28:39 +0100

    added session implementation

---
+++ AssessmentSvc.Core/Interfaces/ISessionSetup.cs
@@ -0,0 +1,17 @@
+﻿using AssessmentSvc.Core.ViewModels.SessionSetup;
+using Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentSvc.Core.Interfaces
+{
+    public interface ISessionSetup
+    {
+        Task<ResultModel<SessionSetupDetail>> AddSchoolSession(AddSessionSetupVM vM);
+        Task<ResultModel<SessionSetupDetail>> UpdateSchoolSession(long Id, AddSessionSetupVM vM);
+        Task<ResultModel<List<SessionSetupList>>> GetSchoolSessionS();
+        Task<ResultModel<SessionSetupDetail>> GetCurrentSchoolSession();
+    }
+}
