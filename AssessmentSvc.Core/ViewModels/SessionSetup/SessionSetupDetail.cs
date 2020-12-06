f022f407ecd9023d381667d45937d62143518750
Frank Ofoedu <fofoedu@sbsc.com>
2020-12-06 11:25:58 +0100

    view models for session setup

---
+++ AssessmentSvc.Core/ViewModels/SessionSetup/SessionSetupDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssessmentSvc.Core.ViewModels.SessionSetup
+{
+    public class SessionSetupDetail
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCurrent { get; set; }
+        public List<TermVM> Terms { get; set; }
+    }
+}
---
+++ AssessmentSvc.Core/ViewModels/SessionSetup/TermVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssessmentSvc.Core.ViewModels.SessionSetup
+{
+    public class TermVM
+    {
+        public int SequenceNumber { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
