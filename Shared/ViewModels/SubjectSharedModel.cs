5f4bf38a0526375fefe7d3372d63281302b5217e
Okpara Obinna <obainoforgod@gmail.com>
2020-12-06 11:26:38 +0100

    Added subject broadcast vm

---
+++ Shared/ViewModels/SubjectSharedModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.ViewModels
+{
+    public class SubjectSharedModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public long TenantId { get; set; }
+    }
+}
