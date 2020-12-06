d928e56500663680ca808741da00df2350fee56e
Okpara Obinna <obainoforgod@gmail.com>
2020-12-06 08:17:16 +0100

    Model maps

--- AssessmentSvc.Core/Models/Map/TeacherMap.cs
+++ AssessmentSvc.Core/Models/Map/TeacherMap.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AssessmentSvc.Core.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace LearningSvc.Core.Models.Map
