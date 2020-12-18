52f002fd2d789d539c77176922638b158ae796bc
Frank Ofoedu <fofoedu@sbsc.com>
2020-12-18 02:03:04 +0100

    implemented broad sheet view

--- AssessmentSvc.API/Controllers/ResultController.cs
+++ AssessmentSvc.API/Controllers/ResultController.cs
@@ -98,5 +98,26 @@ namespace AssessmentSvc.API.Controllers
                 return HandleError(ex);
             }
         }
+
+        [HttpGet("{Id}")]
+        [ProducesResponseType(typeof(ApiResponse<string>), 200)]
+        public async Task<IActionResult> GetClassBroadSheet(long Id)
+        {
+            if (Id < 1)
+                return ApiResponse<string>(errors: "Please provide valid Id", codes: ApiResponseCodes.INVALID_REQUEST);
+
+            try
+            {
+                var result = await _resultService.GetClassBroadSheet(Id);
+
+                if (result.HasError)
+                    return ApiResponse<string>(errors: result.ErrorMessages.ToArray());
+                return ApiResponse(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
     }
 }
