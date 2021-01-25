05145cbc9765ebc0bd3d1d5bdf25053b63269a88
Frank Ofoedu <fofoedu@sbsc.com>
2021-01-25 16:16:55 +0100

    tenancy implementation header

---
+++ Shared/Exceptions/TenantNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Exceptions
+{
+    public class TenantNotFoundException : Exception
+    {
+        public string ErrorCode { get; set; }
+
+        public TenantNotFoundException(string message) : base(message)
+        { }
+
+        public TenantNotFoundException(string message, string errorCode) : base(message)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
