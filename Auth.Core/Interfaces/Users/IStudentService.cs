385176ae8e805881b5d8e7f68844c1ddf6f95bb1
Ofoedu Frank Ebuka <Frankofoedu@gmail.com>
2021-02-11 18:30:33 +0100

    student update implementation

--- Auth.Core/Interfaces/Users/IStudentService.cs
+++ Auth.Core/Interfaces/Users/IStudentService.cs
@@ -17,7 +17,7 @@ namespace Auth.Core.Services.Interfaces
 
         Task<ResultModel<PaginatedModel<StudentVM>>> GetAllStudentsInSchool(QueryModel model);
         Task<ResultModel<StudentDetailVM>> GetStudentById(long Id);
-        Task<ResultModel<StudentVM>> UpdateStudent(StudentUpdateVM model);
+        Task<ResultModel<StudentVM>> UpdateStudent(long Id, StudentUpdateVM model);
         Task<ResultModel<StudentDetailVM>> GetStudentProfileById(long Id);
     }
 }
