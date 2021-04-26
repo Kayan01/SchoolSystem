7a554d61970d73c9b860b7976b96e8247db67c62
Frank Ofoedu <fofoedu@sbsc.com>
2021-04-26 15:49:47 +0100

    school broadcast receive

--- FinanceSvc.API/Startup.Di.cs
+++ FinanceSvc.API/Startup.Di.cs
@@ -20,6 +20,7 @@ using FinanceSvc.Core.Services.Interfaces;
 using FinanceSvc.Core.Services;
 using Shared.Net.WorkerService;
 using Microsoft.Extensions.Logging;
+using FinanceSvc.Core.Interfaces;
 
 namespace FinanceSvc.API
 {
@@ -92,6 +93,14 @@ namespace FinanceSvc.API
                                     await handler.HandleAddOrUpdateSessionAsync(message);
                                     break;
                                 }
+
+                            case (int)BusMessageTypes.SCHOOL:
+                            case (int)BusMessageTypes.SCHOOL_DELETE:
+                            case (int)BusMessageTypes.SCHOOL_UPDATE:
+                                {
+                                    await handler.HandleAddOrUpdateSchoolAsync(message);
+                                    break;
+                                }
                         }
                     }
                     catch (Exception e)
@@ -130,8 +139,11 @@ namespace FinanceSvc.API
             services.AddScoped<IFeeService, FeeService>();
             services.AddScoped<IInvoiceService, InvoiceService>();
             services.AddScoped<ISessionSetupService, SessionSetupService>();
+            services.AddScoped<ISchoolService, SchoolService>();
             services.AddScoped<ITransactionService, TransactionService>();
             services.AddScoped<IFileStore, FileStore>();
+
+            services.AddScoped<IPublishService, PublishService>();
 
             //services.AddTransient<IFileUploadService, FileUploadService>();
             services.AddScoped<IFinanceService, FinanceService>();
