9ee2cb8c6a7e93f4525bf2d5c8517e11a2486f59
Okpara Obinna <obainoforgod@gmail.com>
2021-06-10 09:14:02 +0100

    Assessment service unit test

---
+++ AssessmentSvc.Core.Test/Tests/ApprovedResultService/ApprovedResultService_SubmitStudentResult.cs
@@ -0,0 +1,34 @@
+﻿using AssessmentSvc.Core.Test.Setup;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using AssessmentSvc.Core.Interfaces;
+
+namespace AssessmentSvc.Core.Test.Tests.ApprovedResultService
+{
+    public class ApprovedResultService_SubmitStudentResult
+    {
+        [Test]
+        public async Task SubmitStudentResult_NewResult()
+        {
+            using (ServicesDISetup _setup = new ServicesDISetup())
+            {
+                var approvedService = _setup.ServiceProvider.GetService<IApprovedResultService>();
+                var context = _setup.DbContext;
+
+
+
+                approvedService.SubmitStudentResult(newSchool);
+
+                var schoolCheck = context.Schools.SingleOrDefault(x => x.Id == newSchool.Id);
+
+                Assert.IsFalse(schoolCheck is null);
+                Assert.AreEqual(schoolCheck.Name, newSchool.Name);
+            }
+        }
+
+    }
+}
---
+++ AssessmentSvc.Core.Test/Tests/SubjectService/SubjectService_AddOrUpdateTeacherFromBroadcast.cs
@@ -0,0 +1,76 @@
+﻿using AssessmentSvc.Core.Interfaces;
+using AssessmentSvc.Core.Test.Setup;
+using NUnit.Framework;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Shared.ViewModels;
+using System.Linq;
+using AssessmentSvc.Core.Models;
+
+namespace AssessmentSvc.Core.Test.Tests.SubjectService
+{
+    [TestFixture]
+    public class SubjectService_AddOrUpdateSubjectFromBroadcast
+    {
+
+        [Test]
+        public void AddOrUpdateSubjectFromBroadcast_AddSubject()
+        {
+            using (ServicesDISetup _setup = new ServicesDISetup())
+            {
+                var _subjectService = _setup.ServiceProvider.GetService<ISubjectService>();
+                var context = _setup.DbContext;
+
+                var newSubject = new SubjectSharedModel()
+                {
+                    IsActive = true,
+                    Name = "subject",
+                    Id = 1,
+                };
+
+                _subjectService.AddOrUpdateSubjectFromBroadcast(newSubject);
+
+                var subjectCheck = context.Subjects.SingleOrDefault(x => x.Id == newSubject.Id);
+
+                Assert.IsNotNull(subjectCheck);
+                Assert.AreEqual(newSubject.Name, subjectCheck.Name);
+            }
+        }
+
+        [Test]
+        public void AddOrUpdateSubjectFromBroadcast_UpdateSubject()
+        {
+            using (ServicesDISetup _setup = new ServicesDISetup())
+            {
+                var _subjectService = (ISubjectService)_setup.ServiceProvider.GetService(typeof(ISubjectService));
+                var context = _setup.DbContext;
+
+
+                var oldSubject = new Subject()
+                {
+                    IsActive = true,
+                    Name = "oldsubject",
+                    Id = 1,
+                };
+                context.Add(oldSubject);
+                context.SaveChanges();
+
+                var newSubject = new SubjectSharedModel()
+                {
+                    IsActive = true,
+                    Name = "newsubject",
+                    Id = 1,
+                };
+
+                _subjectService.AddOrUpdateSubjectFromBroadcast(newSubject);
+
+                var subjectCheck = context.Subjects.SingleOrDefault(x => x.Id == newSubject.Id);
+
+                Assert.IsNotNull(subjectCheck);
+                Assert.AreEqual(newSubject.Name, subjectCheck.Name);
+            }
+        }
+    }
+}
