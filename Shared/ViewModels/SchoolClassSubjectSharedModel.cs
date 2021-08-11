3bdef662b15349ce1a929bd7485685f6b2f8ce9c
Okpara Obinna <obainoforgod@gmail.com>
2021-08-11 22:42:50 +0100

    New broadcast object (schoolClass)

---
+++ Shared/ViewModels/SchoolClassSubjectSharedModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.ViewModels
+{
+    public class SchoolClassSubjectSharedModel
+    {
+        public long Id { get; set; }
+        public long TenantId { get; set; }
+        public long SchoolClassId { get; set; }
+        public long SubjectId { get; set; }
+    }
+}
