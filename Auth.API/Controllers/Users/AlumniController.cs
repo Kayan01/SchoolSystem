820fae5cbd7f8b0f391cb3d49215d9f86e0d22ec
Ofoedu Frank Ebuka <Frankofoedu@gmail.com>
2021-08-16 12:01:50 +0100

    alumni service: added implementation to add alumni

---
+++ Auth.API/Controllers/Users/AlumniController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Auth.Core.Services.Interfaces;
+using Auth.Core.ViewModels;
+using Auth.Core.ViewModels.Alumni;
+using Microsoft.AspNetCore.Mvc;
+using Shared.AspNetCore;
+using Shared.AspNetCore.Policy;
+using Shared.Pagination;
+using Shared.Permissions;
+using Shared.ViewModels;
+using Shared.ViewModels.Enums;
+
+
+namespace Auth.API.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
+    public class AlumniController : BaseController
+    {
+
+        private readonly IAlumniService _alumniService;
+        public AlumniController(IAlumniService alumniService)
+        {
+            _alumniService = alumniService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ApiResponse<AlumniDetailVM>), 200)]
+        public async Task<IActionResult> AddAlumni(AddAlumniVM vm)
+        {
+
+            if (vm == null)
+                return ApiResponse<string>(errors: "Empty payload");
+
+            if (!ModelState.IsValid)
+                return ApiResponse<object>(ListModelErrors, codes: ApiResponseCodes.INVALID_REQUEST);
+            try
+            {
+                var result = await _alumniService.AddAlumni(vm);
+
+                if (result.HasError)
+                    return ApiResponse<object>(errors: result.ErrorMessages.ToArray());
+                return ApiResponse(message: "Successful", codes: ApiResponseCodes.OK, data: result.Data);
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+
+        
+
+    }
+}
---
+++ Auth.Core/Interfaces/Users/IAlumni.cs
@@ -0,0 +1,22 @@
+﻿using Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Auth.Core.ViewModels.Staff;
+using Shared.Pagination;
+using Auth.Core.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Auth.Core.ViewModels.Alumni;
+
+namespace Auth.Core.Services.Interfaces
+{
+   public interface IAlumniService
+    {
+        Task<ResultModel<AlumniDetailVM>> AddAlumni(AddAlumniVM vM);
+        Task<ResultModel<PaginatedModel<AlumniDetailVM>>> GetAllAlumni(QueryModel model);
+        Task<ResultModel<AlumniDetailVM>> GetAlumniById(long Id);
+        Task<ResultModel<AlumniDetailVM>> UpdateAlumni(UpdateAlumniVM model );
+        Task<ResultModel<bool>> DeleteAlumni(long Id);
+    }
+}
