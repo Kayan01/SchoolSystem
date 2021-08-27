1f7736b8891db1be7a233ba5187f6d977cc174b0
Okpara Obinna <obainoforgod@gmail.com>
2021-08-27 08:58:10 +0100

    PromotionLog model is added and registered in db context

---
+++ Auth.Core/Models/PromotionLog.cs
@@ -0,0 +1,23 @@
+﻿using Auth.Core.Enumeration;
+using Shared.Entities.Auditing;
+using Shared.Tenancy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auth.Core.Models
+{
+    public class PromotionLog : AuditedEntity<long>, ITenantModelType
+    {
+        public long TenantId { get; set; }
+        public PromotionStatus PromotionStatus { get; set; }
+        public string Reason { get; set; }
+
+        public long StudentId { get; set; }
+        public long SessionSetupId { get; set; }
+        public long SchoolClassId { get; set; }
+
+        public SchoolClass SchoolClass { get; set; }
+        public Student Student { get; set; }
+    }
+}
