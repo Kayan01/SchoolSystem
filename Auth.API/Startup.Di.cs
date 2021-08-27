ed21946c6dab8c17ec0e5c7513517f4e79caa10d
Okpara Obinna <obainoforgod@gmail.com>
2021-08-27 08:50:46 +0100

    Registered IPromotionService

--- Auth.API/Startup.Di.cs
+++ Auth.API/Startup.Di.cs
@@ -139,6 +139,7 @@ namespace Auth.API
             services.AddScoped<ISchoolPropertyService, SchoolPropertyService>();
             services.AddScoped<IAlumniService, AlumniService>();
             services.AddScoped<ISchoolGroupService, SchoolGroupService>();
+            services.AddScoped<IPromotionService, PromotionService>();
             services.AddTransient<AuthHandler>();
 
             // Registers required services for health checks
