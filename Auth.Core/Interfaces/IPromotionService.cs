448ca7e4362babfaa142d3094cbc218b4a3344f6
Okpara Obinna <obainoforgod@gmail.com>
2021-09-13 08:57:53 +0100

    Promotion log endpoints and migration

--- Auth.Core/Interfaces/IPromotionService.cs
+++ Auth.Core/Interfaces/IPromotionService.cs
@@ -1,4 +1,6 @@
-﻿using Shared.ViewModels;
+﻿using Auth.Core.ViewModels.Promotion;
+using Shared.Pagination;
+using Shared.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +11,10 @@ namespace Auth.Core.Interfaces
     public interface IPromotionService
     {
         public Task PromoteAllStudent(PromotionSharedModel model);
+        Task<ResultModel<PaginatedModel<ClassPoolVM>>> GetClassPool(QueryModel vm, long sessionId, long? fromClassId);
+        Task<ResultModel<string>> PostClassPool(List<ClassPoolVM> VMs);
+        Task<ResultModel<PaginatedModel<ClassPoolVM>>> GetWithdrawnList(QueryModel vm, long sessionId, long? fromClassId);
+        Task<ResultModel<PaginatedModel<ClassPoolVM>>> GetRepeatList(QueryModel vm, long sessionId, long? fromClassId);
+        Task<ResultModel<List<PromotionHighlightVM>>> GetPromotionHighlight(long sessionId, long? fromClassId);
     }
 }
---
+++ Auth.Core/ViewModels/Promotion/PromotionHighlightVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auth.Core.ViewModels.Promotion
+{
+    public class PromotionHighlightVM
+    {
+        public long? StudentId { get; set; }
+        public double Score { get; set; }
+    }
+}
