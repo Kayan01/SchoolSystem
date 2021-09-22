cf1f237d237345a93bea58936b05f9310b4474da
Okpara Obinna <obainoforgod@gmail.com>
2021-09-22 16:58:55 +0100

    Subscription invoice

---
+++ Auth.Core/Interfaces/Subscription/ISubscriptionInvoiceService.cs
@@ -0,0 +1,19 @@
+﻿using Auth.Core.ViewModels.Subscription;
+using Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Core.Interfaces
+{
+    public interface ISubscriptionInvoiceService
+    {
+        Task<ResultModel<SubcriptionInvoiceVM>> GetNextSubsciptionInvoice(long schoolId);
+        Task<ResultModel<string>> PostNextSubsciptionInvoice(SubcriptionInvoiceVM invoice);
+        Task<ResultModel<SubcriptionInvoiceVM>> GetArrearsSubsciptionInvoice(long schoolId);
+        Task<ResultModel<string>> PostArrearsSubsciptionInvoice(SubcriptionInvoiceVM invoice);
+        Task<ResultModel<List<GetSubcriptionInvoiceVM>>> GetUnpaidSubsciptionInvoice(long schoolId);
+        Task<ResultModel<string>> MarkInvoiceAsPaid(long invoiceId);
+    }
+}
---
+++ Auth.Core/ViewModels/Subscription/GetSubcriptionInvoiceVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auth.Core.ViewModels.Subscription
+{
+    public class GetSubcriptionInvoiceVM
+    {
+        public long InvoiceId { get; set; }
+        public int NumberOfStudent { get; set; }
+        public int AmountPerStudent { get; set; }
+        public int AmountToBePaid
+        {
+            get
+            {
+                return NumberOfStudent * AmountPerStudent;
+            }
+        }
+        public DateTime DueDate { get; set; }
+        public long SchoolId { get; set; }
+        public string SchoolName { get; set; }
+
+        public bool Paid { get; set; }
+        public DateTime PaidDate { get; set; }
+
+        public string InvoiceType { get; set; }
+    }
+}
